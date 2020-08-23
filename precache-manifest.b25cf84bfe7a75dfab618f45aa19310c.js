7c6258cc3730212dba2312d17286159d21a4aef4
Rowan <rowan@MacBook-Pro-van-Rowan.local>
2020-08-23 16:11:26 +0200

    Updates

---
+++ precache-manifest.b25cf84bfe7a75dfab618f45aa19310c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d3af65285cadc473f5c37df138c454",
+    "url": "/robofriendz/index.html"
+  },
+  {
+    "revision": "7361d71280dd6772114b",
+    "url": "/robofriendz/static/css/main.a7076413.chunk.css"
+  },
+  {
+    "revision": "88363eed96f2d9aa0b51",
+    "url": "/robofriendz/static/js/2.796ad886.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriendz/static/js/2.796ad886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7361d71280dd6772114b",
+    "url": "/robofriendz/static/js/main.4ffc9411.chunk.js"
+  },
+  {
+    "revision": "f0337bff4ab55c1c871c",
+    "url": "/robofriendz/static/js/runtime-main.e09a9c45.js"
+  }
+]);
